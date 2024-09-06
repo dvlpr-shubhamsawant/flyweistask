c2094535823fa20198515dd8c5d9a9a17585b8d0
Neeraj Patil <neeraj@Neerajs-MacBook-Pro.local>
2024-09-06 22:19:09 +0530

    init repo

---
+++ lib/consts/endpoints.dart
@@ -0,0 +1,18 @@
+class Endpoints {
+  static String baseUrl = "https://sanjay-tiwari-backend.vercel.app";
+  static String registerUser = "/api/user/register";
+  static String verifyOtp = "/api/user/verify/668933964bbddd8b44face8e";
+  static String resendOtp = "api/user/resend/650d2bc9540d5ae5cd499869";
+  static String forgetPassword = "api/user/forgetPassword";
+  static String forgetVerifyOtp = "api/user/forgotVerifyotp";
+  static String changePassword =
+      "api/user/changePassword/668933964bbddd8b44face8e";
+  static String getUserProfile = "/api/user/profile";
+  static String getUserProfileById = "/api/user/users/6687dbbc452b9ba48d9f7795";
+  static String updateProfile = "/api/user/update";
+  static String uploadProfilePicture = "/api/user/upload-profile-picture";
+  static String deleteUser = "/api/user/users/65166718a9d6ed4607f5336a";
+  static String uploadId = "api/user/upload-id-picture";
+  static String addShopDetails = "api/user/update-documents";
+  static String updateBankDetails = "api/user/updateBankDetails";
+}
